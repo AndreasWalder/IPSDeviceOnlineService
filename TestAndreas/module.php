8ea68b42ac2c42fed6145bd1eb41385d17cfd1f2
AndreasWalder <andreas.walder@web.de>
2018-05-27 15:45:44 +0200

    test

--- TestAndreas/module.php
+++ TestAndreas/module.php
@@ -9,19 +9,19 @@ class TestAndreas extends IPSModule
         $this->RegisterPropertyString('device1', '');
         $this->RegisterPropertyString('user1', '');
         $this->RegisterPropertyString('macaddress1', '');
-        $this->RegisterPropertyBoolean('active1', '');
+        $this->RegisterPropertyBoolean('active1', 'false');
 		$this->RegisterPropertyString('device2', '');
         $this->RegisterPropertyString('user2', '');
         $this->RegisterPropertyString('macaddress2', '');
-        $this->RegisterPropertyBoolean('active2', '');
+        $this->RegisterPropertyBoolean('active2', 'false');
 		$this->RegisterPropertyString('device3', '');
         $this->RegisterPropertyString('user3', '');
         $this->RegisterPropertyString('macaddress3', '');
-        $this->RegisterPropertyBoolean('active3', '');
+        $this->RegisterPropertyBoolean('active3', 'false');
 		$this->RegisterPropertyString('device4', '');
         $this->RegisterPropertyString('user4', '');
         $this->RegisterPropertyString('macaddress4', '');
-        $this->RegisterPropertyBoolean('active4', '');
+        $this->RegisterPropertyBoolean('active4', 'false');
 		
 		$this->RegisterPropertyString('DebugDevice', '');
     }
