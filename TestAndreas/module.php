55e4ac1ef2c3be015637a4ac43593f40d24242a2
AndreasWalder <andreas.walder@web.de>
2018-05-27 14:08:49 +0200

    Test

--- TestAndreas/module.php
+++ TestAndreas/module.php
@@ -6,40 +6,58 @@ class TestAndreas extends IPSModule
     {
         parent::Create();
 
-        $this->RegisterPropertyString('server', '');
-        $this->RegisterPropertyInteger('port', '3306');
-        $this->RegisterPropertyString('user', '');
-        $this->RegisterPropertyString('password', '');
-        $this->RegisterPropertyString('database', '');
+        $this->RegisterPropertyString('device1', '');
+        $this->RegisterPropertyString('user1', '');
+        $this->RegisterPropertyString('macaddress1', '');
+        $this->RegisterPropertyBoolean('active1', '');
+		$this->RegisterPropertyString('device2', '');
+        $this->RegisterPropertyString('user2', '');
+        $this->RegisterPropertyString('macaddress2', '');
+        $this->RegisterPropertyBoolean('active2', '');
+		$this->RegisterPropertyString('device3', '');
+        $this->RegisterPropertyString('user3', '');
+        $this->RegisterPropertyString('macaddress3', '');
+        $this->RegisterPropertyBoolean('active3', '');
+		$this->RegisterPropertyString('device4', '');
+        $this->RegisterPropertyString('user4', '');
+        $this->RegisterPropertyString('macaddress4', '');
+        $this->RegisterPropertyBoolean('active4', '');
+		
+		$this->RegisterPropertyString('DebugDevice', '');
     }
 
     public function ApplyChanges()
     {
         parent::ApplyChanges();
 
-        $server = $this->ReadPropertyString('server');
-        $port = $this->ReadPropertyInteger('port');
-        $user = $this->ReadPropertyString('user');
-        $password = $this->ReadPropertyString('password');
-        $database = $this->ReadPropertyString('database');
+        $device1 = $this->ReadPropertyString('device1');
+        $user1 = $this->ReadPropertyString('user1');
+        $macaddress1 = $this->ReadPropertyString('macaddress1');
+        $active1 = $this->ReadPropertyBoolean('active1');
+		$device1 = $this->ReadPropertyString('device2');
+        $user1 = $this->ReadPropertyString('user2');
+        $macaddress1 = $this->ReadPropertyString('macaddress2');
+        $active1 = $this->ReadPropertyBoolean('active2');
+		$device1 = $this->ReadPropertyString('device3');
+        $user1 = $this->ReadPropertyString('user3');
+        $macaddress1 = $this->ReadPropertyString('macaddress3');
+        $active1 = $this->ReadPropertyBoolean('active3');
+		$device1 = $this->ReadPropertyString('device4');
+        $user1 = $this->ReadPropertyString('user4');
+        $macaddress1 = $this->ReadPropertyString('macaddress4');
+        $active1 = $this->ReadPropertyBoolean('active4');
+		
+		$DebugDevice = $this->ReadPropertyString('DebugDevice');
 
-        if ($server != '' && $port > 0) {
-            $ok = true;
-            if ($server == '') {
-                echo 'no value for property "server"';
-                $ok = false;
+        if ($device1 != '' && $user1 != '') {
+            $ok1 = true;
+            if ($device1 == '') {
+                echo 'no value for property "device1"';
+                $ok1 = false;
             }
-            if ($user == '') {
-                echo 'no value for property "user"';
-                $ok = false;
-            }
-            if ($password == '') {
-                echo 'no value for property "password"';
-                $ok = false;
-            }
-            if ($database == '') {
-                echo 'no value for property "database"';
-                $ok = false;
+            if ($user1 == '') {
+                echo 'no value for property "user1"';
+                $ok1 = false;
             }
             $this->SetStatus($ok ? 102 : 201);
         } else {
@@ -47,106 +65,9 @@ class TestAndreas extends IPSModule
         }
     }
 
-    public function TestConnection()
+    public function DebugDevice()
     {
-        $rows = $this->ExecuteSimple('select now() as now');
-        if ($rows && isset($rows[0]->now)) {
-            $now = strtotime($rows[0]->now);
-            $tstamp = date('d.m.Y H:i:s', $now);
-            $n = $now - time();
-            if (abs($n) > 1) {
-                $msg = ', differ from localtime ' . $n . 'sec';
-            } else {
-                $msg = '';
-            }
-            echo 'current timestamp on database-server is ' . $tstamp . $msg;
+            echo 'DebugDevice true';
             $this->SetStatus(102);
-        } else {
-            $this->SetStatus(201);
-        }
-    }
-
-    public function Open()
-    {
-        $server = $this->ReadPropertyString('server');
-        $port = $this->ReadPropertyInteger('port');
-        $user = $this->ReadPropertyString('user');
-        $password = $this->ReadPropertyString('password');
-        $database = $this->ReadPropertyString('database');
-
-        $this->SendDebug(__FUNCTION__, 'open database ' . $database . '@' . $server . ':' . $port . '(user=' . $user . ')', 0);
-
-        $dbHandle = new mysqli($server, $user, $password, $database);
-        if ($dbHandle->connect_errno) {
-            $this->SendDebug(__FUNCTION__, " => can't open database", 0);
-            echo "can't open database " . $database . '@' . $server . ': ' . $dbHandle->connect_error . "\n";
-
-            return false;
-        }
-
-        $this->SendDebug(__FUNCTION__, ' => dbHandle=' . print_r($dbHandle, true), 0);
-
-        return $dbHandle;
-    }
-
-    public function Close(int $dbHandle)
-    {
-        if ($dbHandle && $dbHandle->close() == false) {
-            echo "unable to close database\n";
-
-            return false;
-        }
-
-        return true;
-    }
-
-    public function ExecuteSimple(string $statement)
-    {
-        $server = $this->ReadPropertyString('server');
-        $database = $this->ReadPropertyString('database');
-
-        $dbHandle = $this->Open();
-        if ($dbHandle == false) {
-            return $dbHandle;
-        }
-
-        $ret = $this->Query($dbHandle, $statement);
-
-        return $ret;
-    }
-
-    public function Query(int $dbHandle, string $statement)
-    {
-        $server = $this->ReadPropertyString('server');
-        $database = $this->ReadPropertyString('database');
-
-        if ($dbHandle == false) {
-            echo 'unable to execute statement "' . $statement . "\": invalid database-handle\n";
-
-            return $dbHandle;
-        }
-
-        $this->SendDebug(__FUNCTION__, 'query "' . $statement . '" on ' . $database . '@' . $server, 0);
-        $res = $dbHandle->query($statement);
-        if ($res == false) {
-            $this->SendDebug(__FUNCTION__, ' => unable to query', 0);
-            echo 'unable to execute statement "' . $statement . '": ' . $dbHandle->error . "\n";
-
-            return $res;
-        }
-
-        if (!isset($res->num_rows)) {
-            return $res;
-        }
-
-        $this->SendDebug(__FUNCTION__, ' => got ' . $res->num_rows . ' rows', 0);
-
-        $rows = [];
-        while ($row = $res->fetch_object()) {
-            $rows[] = $row;
-        }
-        $res->close();
-
-        return $rows;
     }
 }
