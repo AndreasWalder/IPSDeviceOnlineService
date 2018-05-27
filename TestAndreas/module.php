da3743bceaefa6e01bbf5010405ecaff7691ea7e
AndreasWalder <andreas.walder@web.de>
2018-05-28 00:02:01 +0200

    sdsd

--- TestAndreas/module.php
+++ TestAndreas/module.php
@@ -91,7 +91,46 @@ class TestAndreas extends IPSModule
     }
 
          public function UpdateData() {
-			 echo 'Hallo';
+			 echo 'Hallo'
+			 
+			 $device1 = $this->ReadPropertyString('device1');
+        $user1 = $this->ReadPropertyString('user1');
+        $macaddress1 = $this->ReadPropertyString('macaddress1');
+        $active1 = $this->ReadPropertyBoolean('active1');
+		$device1 = $this->ReadPropertyString('device2');
+        $user1 = $this->ReadPropertyString('user2');
+        $macaddress1 = $this->ReadPropertyString('macaddress2');
+        $active1 = $this->ReadPropertyBoolean('active2');
+		$device1 = $this->ReadPropertyString('device3');
+        $user1 = $this->ReadPropertyString('user3');
+        $macaddress1 = $this->ReadPropertyString('macaddress3');
+        $active1 = $this->ReadPropertyBoolean('active3');
+		$device1 = $this->ReadPropertyString('device4');
+        $user1 = $this->ReadPropertyString('user4');
+        $macaddress1 = $this->ReadPropertyString('macaddress4');
+        $active1 = $this->ReadPropertyBoolean('active4');
+		
+		$DebugDeviceName = $this->ReadPropertyString('DebugDeviceName');
+		
+		$this->SetTimerInterval("Update", $this->ReadPropertyInteger("UpdateInterval")*1000*60);
+
+		if ($device1 != '' && $user1 != '') {
+               $ok1 = true;
+               if ($device1 != '') {
+                 echo 'no value for property "device1"';
+                 $ok1 = false;
+                 }
+               if ($user1 != '') {
+                echo 'no value for property "user1"';
+                $ok1 = false;
+               }
+               $this->SetStatus($ok ? 102 : 201);
+        } 
+		else {
+             $this->SetStatus(104);
+        }
+			 
+			 
 		 
 		 }
 		 
