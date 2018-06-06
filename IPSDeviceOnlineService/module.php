05ae93992495fff93f038a39a950b84a7365a5bd
AndreasWalder <andreas.walder@web.de>
2018-06-06 19:55:37 +0200

    df

--- IPSDeviceOnlineService/module.php
+++ IPSDeviceOnlineService/module.php
@@ -32,28 +32,28 @@ class IPSDeviceOnlineService extends IPSModule
         $this->RegisterPropertyString('user1', '');
         $this->RegisterPropertyString('macaddress1', '');
 		$this->RegisterPropertyString('hostname1', '');
-		$this->RegisterPropertyInteger('dhcpType1', 0);	
+		//$this->RegisterPropertyInteger('dhcpType1', 0);	
         $this->RegisterPropertyBoolean('active1', 'false');
 		
 		$this->RegisterPropertyString('device2', '');
         $this->RegisterPropertyString('user2', '');
         $this->RegisterPropertyString('macaddress2', '');
 		$this->RegisterPropertyString('hostname2', '');
-		$this->RegisterPropertyInteger('dhcpType2', 0);	
+		//$this->RegisterPropertyInteger('dhcpType2', 0);	
         $this->RegisterPropertyBoolean('active2', 'false');
 		
 		$this->RegisterPropertyString('device3', '');
         $this->RegisterPropertyString('user3', '');
         $this->RegisterPropertyString('macaddress3', '');
 		$this->RegisterPropertyString('hostname3', '');
-		$this->RegisterPropertyInteger('dhcpType3', 0);	
+		//$this->RegisterPropertyInteger('dhcpType3', 0);	
         $this->RegisterPropertyBoolean('active3', 'false');
 		
 		$this->RegisterPropertyString('device4', '');
         $this->RegisterPropertyString('user4', '');
         $this->RegisterPropertyString('macaddress4', '');
 		$this->RegisterPropertyString('hostname4', '');
-		$this->RegisterPropertyInteger('dhcpType4', 0);	
+		//$this->RegisterPropertyInteger('dhcpType4', 0);	
         $this->RegisterPropertyBoolean('active4', 'false');
 		
 		$this->RegisterPropertyString('DebugDeviceAddress', '');
